d8331084f972e5e44352521c0cb6849c93b63018
maxiecuti <juhjuh.karla@gmail.com>
2025-04-01 10:11:30 -0300

    editei o data.sql o id do usuario

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,7 +12,7 @@ INSERT IGNORE INTO tb_categoria_produto (id_categoria_produto, nome_categoria_pr
 (5, 'Relógios', 'Acesório para não atrasar no SENAI');
 
 -- Inserindo Tipos (se não existirem)
-INSERT IGNORE INTO tb_tipo_produto (idTipo_produto, nome_tipo_produto, descricao_tipo_produto) VALUES
+INSERT IGNORE INTO tb_tipo_produto (id_tipo_produto, nome_tipo_produto, descricao_tipo_produto) VALUES
 (1, 'Ouro', '18k'),
 (2, 'Prata', '750'),
 (3, 'Bijuteria', 'É biju mas é de rico');
